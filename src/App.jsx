882327e26a00f1cbe55bd16a69db6457d2dbb6a3
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-02-09 16:41:43 +0530

    Today face some problem releted font

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css';
+import Navbar from './Component/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App bg-blue-600 text-center">
-      <h1 className="bg-orange-400">Hello</h1>
-  
-    </div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
